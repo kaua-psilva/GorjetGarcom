621f0532dd167886f6d4c0ec7c4b8d79cd6690c5
kaua-psilva <kauapaulinoo2005@gmail.com>
2024-11-23 20:47:01 -0400

    Atualização de bg, código Js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,3 @@
-// CALCULAR GORJÉTA
-
-
-
 // LOCALIZANDO OS INPUTS
 let valorConta = document.getElementById("billValue");
 let customGorjeta = document.getElementById("customValue");
@@ -13,7 +9,7 @@ function calcularGorjeta() {
     // NOVAS VARIÁVEIS PARA ESTAR DEFININDO OS VALORES PADRÃO DOS INPUT
     const contaUser = parseFloat(valorConta.value) || 0;
     let porcentage = parseInt(customGorjeta.value) || 0;
-    const QtdPessoas = parseInt(numPessoas.value) || 1;
+    const QtdPessoas = parseFloat(numPessoas.value) || 1;
 
 
     let Buttons = ['btn1', 'btn2', 'btn3', 'btn4', 'btn5'].map(id => document.getElementById(id));
@@ -43,11 +39,24 @@ function calcularGorjeta() {
         const totalGorjetaPorPessoa = (contaUser * (porcentage / 100)) / QtdPessoas;
         const totalPorPessoa = (contaUser + totalGorjetaPorPessoa) / QtdPessoas;
 
-        document.getElementById('totalContaUser').innerText = totalGorjetaPorPessoa.toFixed(2);
-        document.getElementById('totalGorjeta').innerText = totalPorPessoa.toFixed(2);
+        document.getElementById('totalContaUser').innerText = totalGorjetaPorPessoa.toFixed(2) + "R$";
+        document.getElementById('totalGorjeta').innerText = totalPorPessoa.toFixed(2) + "R$";
     }
     calcularTotais();
+    // VERIFICAR VALORES 0 Á CIMMA DE 0
+    if(contaUser <= 0){
+        alert("Insira somente valores superiores á 0")
+    }else{
+        log("Valores válidos")
+    }
+    calcularTotais()
 }
+
+
+
 valorConta.addEventListener('input', calcularGorjeta);
 numPessoas.addEventListener('input', calcularGorjeta);
 customGorjeta.addEventListener('input', calcularGorjeta);
+
+
+
